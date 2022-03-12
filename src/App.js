7a7574035037daa83c4ef070b6eb5a7f8ebffc08
EmmaWLC <weilin.emma.chen@gmail.com>
2022-03-12 22:18:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import { Router, Route, Switch } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
+import { Icon } from "@iconify/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <div className="search__box">
+          <input type="text" />
+        </div>
+        <div className="search__icon">
+          <Icon icon="akar-icons:search" />
+        </div>
+      </div>
+      <div className="videoDisplay">
+        <div className="videoDisplay__img"></div>
+        <div className="videoDisplay__context">
+          <h1>Title</h1>
+          <div className="videoDisplay__context__detail">
+            <div className="videoDisplay__context__detail__review">觀看次數：</div>
+            <div className="videoDisplay__context__detail__time">小時前</div>
+          </div>
+          <div className="videoDisplay__channel">
+            <div className="videoDisplay__channel__img">img</div>
+            <div className="videoDisplay__channel__name">TVBS</div>
+          </div>
+          <div className="videoDisplay__context__description">
+            <p>jiji</p>
+          </div>
+          <div className="videoDisplay__context__tag">
+            <p>新影片</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
